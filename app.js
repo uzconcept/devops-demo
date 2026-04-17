4d27a9d5c2eeb105b187b648f92aa1f5aa9975e9
Uzochukwu Iwuagwu <uzoiwuagwu@gmail.com>
2026-04-17 21:42:43 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from DevOps pipeline 🚀");
+});
+
+app.listen(3000, () => console.log("Server running"));
