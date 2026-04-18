74befbbde4189db561f72e9c2e345cb3c3887c03
Uzochukwu Iwuagwu <uzoiwuagwu@gmail.com>
2026-04-18 11:05:54 +0200

    fix runtime compatibility

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("Hello from DevOps pipeline 🚀");
 });
 
-app.listen(3000, () => console.log("Server running"));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
