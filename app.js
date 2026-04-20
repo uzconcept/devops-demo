993e02d82a758f937ad7ba343b813935b68dac9a
Uzochukwu Iwuagwu <uzoiwuagwu@gmail.com>
2026-04-20 10:16:03 +0200

    version 2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from DevOps pipeline 🚀");
+  res.send("Hello from version 2 Super excited to do DEVOPS, Thank you Chatgpt 🚀");
 });
 
 app.listen(port, () => {
